afea8979124d3b3b6e542cedca7ba6d9d60bf09c
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-11-16 01:30:33 -0400

    adds set up for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       devise_scope :user do
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
         post "sign_in", to: "sessions#create"
         delete "log_out", to: "sessions#destroy"
       end
+
+      post "facebook", to: "users#facebook"
+      resources :books, only: [:index, :show] do
+        resources :reviews, only: [:index, :show, :create, :update, :destroy]
+      end
+    end
+  end
 end
